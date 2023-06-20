c57b07fd7a0f8faf7849b5e330b0df231d2a93f8
Tumasang654809254 <tumasang09@gmail.com>
2023-06-20 03:56:14 +0100

    updated appspec.yml file 2

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/bash
-
-# Install node.js
-sudo curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash
-sudo yum install nodejs -y
-
-# Install pm2
-sudo npm install pm2@latest -g
+#!/bin/bash
+# Install node.js and PM2 globally
+sudo apt-get update
+sudo apt-get install nodejs-legacy -y
+sudo apt-get install npm  -y
+sudo npm install pm2 -g
---
+++ scripts/fix_privileges.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir /home/ec2-user/nodejs
+cd /home/ec2-user/nodejs
+sudo npm install
