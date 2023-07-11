234c2a375220ee3d653eeaa2cf964c2883f705e9
Tumasang654809254 <tumasang09@gmail.com>
2023-07-11 02:50:11 +0100

    new yaml files

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
+echo starting server
 
-# navigate to app folder
-cd app
+# here we just use npm to run the build
+cd /var/www/
+echo building application...
+sudo npm run build
 
-# initial startup by running react-script "start", name process "marketing"
-# --watch watches and restarts if files change
-pm2 start ./node_modules/react-scripts/scripts/start.js --name "marketing" --watch
-
-# auto restart server if shut down
-pm2 startup
-
-# freeze process list for automatic respawn
-pm2 save
-
-# restart all processes - necessary to do this again?
-pm2 restart all
+# start the application with pm2
+echo starting application...
+sudo pm2 start npm -- run production
